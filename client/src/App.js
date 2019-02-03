a8cdbcb38a6d761bc3b85328b151c034e569cb6e
leviwp48 <leviwp48@gmail.com>
2019-02-02 23:34:32 -0800

    Adding in React Router and Redux

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,11 @@ import AppNavbar from './components/Nav/AppNavbar';
 import ProjectList from './components/Projects/ProjectList';
 import ProjectModal from './components/Projects/ProjectModal';
 import { Container } from 'reactstrap';
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {BrowserView, MobileView} from "react-device-detect";
+
+import Main from './components/Main';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -10,16 +15,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <AppNavbar />
-          <Container>
-            <ProjectModal />
-            <ProjectList />
-          </Container>
-        </div>
+        <Main /> 
       </Provider>
     );
   }
--- client/src/components/Main.js
+++ client/src/components/Main.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import AppNavbar from '../Nav/AppNavbar';
-import ProjectList from '../Projects/ProjectList';
-import ProjectModal from '../Projects/ProjectModal';
+import AppNavbar from './Nav/AppNavbar';
+import ProjectList from './Projects/ProjectList';
+import ProjectModal from './Projects/ProjectModal';
 import { Container } from 'reactstrap';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {BrowserView, MobileView} from "react-device-detect";
-
+import HomePage from "./Home/HomePage";
 import { Provider } from 'react-redux';
-import store from '../../store';
+import store from '../store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class HomePage extends Component {
+class Main extends Component {
     state = {
       isOpen: false
     };
@@ -19,10 +19,17 @@ class HomePage extends Component {
     render() {
       return (
       <Provider store={store}>
-        <div> hey there </div>    
+        <BrowserView>
+        <AppNavbar />
+          <Router>
+            <Switch>
+              <Route path="/" component={HomePage}/> 
+           </Switch>
+          </Router>
+        </BrowserView>     
       </Provider>
        );
     }
 }
 
-export default HomePage;
+export default Main;
