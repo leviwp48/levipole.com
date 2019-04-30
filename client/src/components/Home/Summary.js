6af55d906584294dd46084104bf2df8fa2d66854
Levi W Pole <lwp41@humboldt.edu>
2019-04-29 19:53:41 -0700

    converted list to listgroup componenet

--- client/src/components/Home/Summary.js
+++ client/src/components/Home/Summary.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Row, Col} from 'reactstrap';
+import { Container, Jumbotron, Row, Col, ListGroupItem, ListGroup} from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../../styles/home/Summary.css'
 
@@ -14,58 +14,58 @@ class Summary extends Component {
 
     render() {
       return (
-        <Container fluid="true" className="Header" style={{backgroundColor: '#FFFFFF', color: '#000000', height: '100vh'}}>   
+        <Jumbotron fluid  style={{backgroundColor: '#FFFFFF', color: '#000000', height: '80vh'}}>   
+        <Container fluid>
          <Row>
-           <Col md = "auto">
-             <h1 style={{fontSize: '40px', marginLeft: '125px', marginTop: "10vh"}} className="display-3">
+           <Col sm={{size:12}}>
+              <h1 className="lead" style={{fontSize:'50px'}}>
                 ABOUT ME  
-             </h1>     
+              </h1>
            </Col>       
          </Row>
          <Row>
-         <Col md={{size:'3', offset: 1}}>
-             <h1 style={{fontSize: '20px', marginTop: '50px'}} className="display-3">Software Engineering</h1> 
-             <Row>
-               <ul>
-                 <li>Implementation of datastructures and algorithms</li> 
-                 <li>Multiple android applications that utilize the many tools of android
-	                   along with external API's</li>
-                 <li>Production of responsive web applications.</li>
-               </ul>
-             </Row>      
+            <Col sm={{size:12}}>
+              <hr className="my-2" />
+            </Col>
+         </Row>
+
+         <Row>
+          <Col sm={{size:3, offset: 1}}>
+             <h1 style={{fontSize: '30px'}} className="lead">Software Engineering</h1> 
+               <ListGroup flush>
+                 <ListGroupItem>Implementation of datastructures and algorithms</ListGroupItem> 
+                 <ListGroupItem>Multiple android applications that utilize the many tools of android
+	                   along with external API's</ListGroupItem>
+                 <ListGroupItem>Production of responsive web applications.</ListGroupItem>
+               </ListGroup>
            </Col>     
-           <Col md={{size:'3'}}>
-             <h1 style={{fontSize: '20px', marginTop: '50px'}} className="display-3">Game Development</h1> 
-             <Row>
-               <ul>
-                 <li>Experience using Unity to create a 2d platformer. Created a custom UI, 
+           <Col md={{size:3}}>
+             <h1 style={{fontSize: '30px'}} className="lead">Game Development</h1> 
+               <ListGroup flush>
+                 <ListGroupItem>Experience using Unity to create a 2d platformer. Created a custom UI, 
                      custom scripts in C#, utilized Unity's physics environment, and 
-                     made assets my own.</li> 
-                 <li>Created a 2d Chess game that programmically creates the game board and pieces. 
-                     Works like ches.</li>
-               </ul>
-             </Row>      
+                     made assets my own.</ListGroupItem> 
+                 <ListGroupItem>Created a 2d Chess game that programmically creates the game board and pieces. 
+                     Works like ches.</ListGroupItem>
+               </ListGroup>
            </Col> 
-           <Col md={{size:'3'}}>
-             <h1 style={{fontSize: '20px', marginTop: '50px'}} className="display-3">Languages</h1> 
-             <Row>
-               <ul>
-                 <li>Proficient: C++, C#, HTML, CSS, JavaScript (React, Node) </li> 
-                 <li>Familiar: Java, SQL, PHP</li>
-               </ul>
-             </Row>      
+           <Col md={{size:3}}>
+             <h1 style={{fontSize: '30px'}} className="lead">Languages</h1> 
+               <ListGroup flush>
+                 <ListGroupItem>Proficient: C++, C#, HTML, CSS, JavaScript (React, Node) </ListGroupItem> 
+                 <ListGroupItem>Familiar: Java, SQL, PHP</ListGroupItem>
+               </ListGroup>
            </Col> 
-           <Col sm={{size:'3', offset: 1}}>
-             <h1 style={{fontSize: '20px', marginTop: '10px'}} className="display-3">Technologies</h1> 
-             <Row>
-               <ul>
-                 <li>Proficient: Android Studio, Unity, phpMyAdmin, Git, Unix Bash, MERN, MySQL, MongoDB, AWS</li> 
-                 <li>Familiar: Oracle, Laravel, LAMP, Heroku, Tensor Flow </li>
-               </ul>
-             </Row>      
+           <Col sm={{size:3, offset:1}}>
+             <h1 style={{fontSize: '30px', marginTop: '30px'}} className="lead">Technologies</h1> 
+               <ListGroup flush>
+                 <ListGroupItem>Proficient: Android Studio, Unity, phpMyAdmin, Git, Unix Bash, MERN, MySQL, MongoDB, AWS</ListGroupItem> 
+                 <ListGroupItem>Familiar: Oracle, Laravel, LAMP, Heroku, Tensor Flow </ListGroupItem>
+               </ListGroup>
            </Col>   
-         </Row>                    
-        </Container>  
+         </Row>    
+         </Container>                
+        </Jumbotron>  
        );
     }
 }
